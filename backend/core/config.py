801a553a4a3fe5a5ea34afa8112fc0f1c707c795
TimaDootaliev <timur.dootaliev.td@gmail.com>
2022-05-10 16:14:20 +0600

    database

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,18 @@
+from decouple import config
+
+
+
 class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+
+    POSTGRES_USER: str = config('POSTGRES_USER')
+    POSTGRES_PASSWORD: str = config('POSTGRES_PASSWORD')
+    POSTGRES_SERVER: str = config('POSTGRES_SERVER')
+    POSTGRES_PORT: str = config('POSTGRES_PORT', 5432)
+    POSTGRES_DB: str = config('POSTGRES_DB', 'tdd')
+    DATABASE_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+# TODO: подключить базу данных
 
 settings = Settings()
