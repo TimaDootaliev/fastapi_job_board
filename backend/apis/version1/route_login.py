1a6f40d719218746c1b394e4986ddfc971f34511
TimaDootaliev <timur.dootaliev.td@gmail.com>
2022-05-24 12:31:59 +0600

    login

--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -1,16 +1,20 @@
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from fastapi import Depends, APIRouter
-from sqlalchemy.orm import Session
 from datetime import timedelta
-from fastapi import status, HTTPException
 
-from db.session import get_db
-from core.hashing import Hasher
-from schemas.token import Token
-from db.repository.login import get_user
-from core.security import create_access_token
 from core.config import settings
-from jose import jwt, JWTError
+from core.hashing import Hasher
+from core.security import create_access_token
+from db.repository.login import get_user
+from db.session import get_db
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from jose import JWTError
+from schemas.token import Token
+from sqlalchemy.orm import Session
 
 
 router = APIRouter()
