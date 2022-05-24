1ac4bc39e135cecd87605e4c9f0c986ec5e39a09
TimaDootaliev <timur.dootaliev.td@gmail.com>
2022-05-24 10:21:16 +0600

    settings

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -4,6 +4,7 @@ from db.repository.jobs import create_new_job
 from db.repository.jobs import list_jobs
 from db.repository.jobs import retrieve_job
 from db.repository.jobs import update_job_by_id
+from db.repository.jobs import delete_job_by_id
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
@@ -44,3 +45,12 @@ def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
     if not message:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id {id} not found")
     return {'msg': 'Successfully updated'}
+
+
+@router.delete('/delete/{id}')
+def delete_job(id: int, db: Session = Depends(get_db)):
+    current_user_id = 1
+    message = delete_job_by_id(id=id, db=db, owner_id=current_user_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Job with id {id} not found')
+    return {'msg': 'Successfully deleted'}
--- backend/core/config.py
+++ backend/core/config.py
@@ -2,6 +2,7 @@ from decouple import config
 
 
 class Settings:
+    """ Settings class """
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
 
@@ -11,6 +12,10 @@ class Settings:
     POSTGRES_PORT: str = config('POSTGRES_PORT', 5432)
     POSTGRES_DB: str = config('POSTGRES_DB', 'tdd')
     DATABASE_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+    SECRET_KEY: str = config('SECRET_KEY')
+    ALGORITHM = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30 # mins
 
 
 settings = Settings()
